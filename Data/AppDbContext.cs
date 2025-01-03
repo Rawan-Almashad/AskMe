c030d58cca1863eba5f7863521bdeffeff0f0834
Rawan-Almashad <rawanalmashad@gmail.com>
2025-01-03 18:48:15 +0200

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AskMeProgram.Configuration;
+using AskMeProgram.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+namespace AskMeProgram.Data
+{
+    public  class AppDbContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Threadd> Threadds { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var config=new ConfigurationBuilder().AddJsonFile("appsetting.json").Build();
+            var connectionstring = config.GetSection("constr").Value;
+            optionsBuilder.UseSqlServer(connectionstring);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(UserConfiguration).Assembly);
+        }
+
+    }
+}
